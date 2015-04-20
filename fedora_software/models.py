7d00a1656ce09ad6c18b16c5ed50bcf6bf965c86
Jakub Dorňák <jdornak@redhat.com>
2015-04-20 18:08:54 +0200

    models

--- fedora_software/models.py
+++ fedora_software/models.py
@@ -1,2 +1,110 @@
 from django.db import models
 
+
+class Category(models.Model):
+    pass
+
+
+
+class CategoryName(models.Model):
+    category    = models.ForeignKey(Category, related_name='names')
+    lang        = models.CharField(max_length=100)
+    name        = models.CharField(max_length=100)
+
+    class Meta:
+        unique_together     = [('category', 'lang')]
+
+
+
+class Component(models.Model):
+    type        = models.CharField(max_length=100)
+    type_id     = models.CharField(max_length=100)
+    pkgname     = models.CharField(max_length=100)
+    categories  = models.ManyToManyField(Category)
+    project_license = models.TextField(null=True)
+
+
+
+class ComponentName(models.Model):
+    component   = models.ForeignKey(Component, related_name='names')
+    lang        = models.CharField(max_length=100)
+    name        = models.CharField(max_length=100)
+
+    class Meta:
+        unique_together     = [('component', 'lang')]
+
+
+
+class ComponentSummary(models.Model):
+    component   = models.ForeignKey(Component, related_name='summaries')
+    lang        = models.CharField(max_length=100)
+    summary     = models.CharField(max_length=100)
+
+    class Meta:
+        unique_together     = [('component', 'lang')]
+
+
+
+class ComponentDescription(models.Model):
+    component   = models.ForeignKey(Component, related_name='descriptions')
+    lang        = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        unique_together     = [('component', 'lang')]
+
+
+
+class ComponentIcon(models.Model):
+    component   = models.ForeignKey(Component, related_name='icons')
+    type        = models.CharField(max_length=100)
+    icon        = models.CharField(max_length=100)
+    height      = models.IntegerField()
+    width       = models.IntegerField()
+
+
+
+class ComponentUrl(models.Model):
+    component   = models.ForeignKey(Component, related_name='urls')
+    type        = models.CharField(max_length=100)
+    url         = models.CharField(max_length=100)
+
+
+
+class ComponentScreenshot(models.Model):
+    component   = models.ForeignKey(Component, related_name='screenshots')
+    type        = models.CharField(max_length=100, null=True)
+
+
+
+class ComponentScreenshotImage(models.Model):
+    screenshot  = models.ForeignKey(ComponentScreenshot, related_name='images')
+    type        = models.CharField(max_length=100, null=True)
+    height      = models.IntegerField(null=True)
+    width       = models.IntegerField(null=True)
+    url         = models.CharField(max_length=1000)
+
+
+
+class ComponentRelease(models.Model):
+    component   = models.ForeignKey(Component, related_name='releases')
+    version     = models.CharField(max_length=100)
+    timestamp   = models.DateTimeField()
+
+
+
+class ComponentLanguage(models.Model):
+    component   = models.ForeignKey(Component, related_name='languages')
+    percentage  = models.IntegerField(null=True)
+    lang        = models.CharField(max_length=100)
+
+
+
+class ComponentMetadata(models.Model):
+    component   = models.ForeignKey(Component, related_name='metadata')
+    key         = models.CharField(max_length=100)
+    value       = models.CharField(max_length=100, null=True)
+
+    class Meta:
+        unique_together     = [('component', 'key')]
+
