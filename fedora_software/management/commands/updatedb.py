760aa52b83966c084ef68b00c0ac5b51d3babc2e
Jakub Dorňák <jdornak@redhat.com>
2015-04-20 16:20:37 +0200

    find xml file in rpm database

--- fedora_software/management/commands/updatedb.py
+++ fedora_software/management/commands/updatedb.py
@@ -1,11 +1,8 @@
 import logging
 import os
-
-from optparse import make_option
+import sys
 
 from django.core.management.base import CommandError
-
-from multiprocessing import Pool, cpu_count
 
 from . import LoggingBaseCommand
 from ...models import models
@@ -15,10 +12,30 @@ logger = logging.getLogger(__name__)
 
 
 class Command(LoggingBaseCommand):
-    help = 'Import data from appstream-data.'
+    args = '<xml file>'
+    help = 'Import data from appstream-data. '
 
     def handle(self, *args, **options):
         self.configure_logging(options['verbosity'])
-        errors = 0
-        self.stdout.write('It works!')
+
+        # check arguments
+        if len(args) == 1:
+            xml_file = args[0]
+        elif len(args) == 0:
+            try:
+                # try to find the file in rpm database
+                import rpm
+                header = rpm.TransactionSet().dbMatch('name', 'appstream-data').next()
+                xml_file = filter(lambda f: f[0].endswith('.xml.gz'), header.fiFromHeader())[0][0]
+            except:
+                raise CommandError(
+                    'Failed to find xml file provided by appstream-data package. '\
+                    'Specify path to the file as an argument.\nType {} help updatedb'.format(
+                        os.path.basename(sys.argv[0])
+                    ))
+        elif len(args) > 1:
+            raise CommandError('Invalid number of arguments.\nType {} help updatedb'.format(
+                os.path.basename(sys.argv[0])))
+
+        logger.info('Reading %s' % xml_file)
 
