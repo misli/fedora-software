ea484684b1f4820651c766165202c0f1de9b654e
Jakub Dorňák <jdornak@redhat.com>
2015-04-24 00:14:50 +0200

    fix language selector

--- fedora_software/urls.py
+++ fedora_software/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import patterns, include, url
-from django.conf.urls.i18n import i18n_patterns
 
 from .views import HomeView, AppView, SearchView, CategoryView, FaqView
 
 #from django.contrib import admin
 #admin.autodiscover()
 
-urlpatterns = i18n_patterns('',
+urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^apps/(?P<id>.*)$', AppView.as_view(), name='app'),
     url(r'^search/.*$', SearchView.as_view(), name='search'),
-    url(r'^category/.*$', CategoryView.as_view(), name='category'),
+    url(r'^category/(?P<slug>.*)$', CategoryView.as_view(), name='category'),
     url(r'^faq$', FaqView.as_view(), name='faq'),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
 )
