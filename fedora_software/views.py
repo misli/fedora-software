8926f087135b5990f9cfc54075db59951cb853c8
Jakub Dorňák <jdornak@redhat.com>
2015-04-23 16:22:22 +0200

    AppView to send app variable to app.html

--- fedora_software/views.py
+++ fedora_software/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 
-from .models import FeaturedApp
+from .models import FeaturedApp, Component
+
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -11,8 +12,24 @@ class HomeView(TemplateView):
             'featured_app': FeaturedApp.objects.order_by('?').first(),
         }
 
-class AppView(TemplateView):
+
+
+class AppView(DetailView):
+    model = Component
     template_name = 'app.html'
+
+    def get_object(self, queryset=None):
+        if queryset is None:
+            queryset = self.get_queryset()
+        return queryset.get(
+            type='desktop',
+            type_id='{}.desktop'.format(self.kwargs['id']),
+        )
+
+    def get_context_object_name(self, obj):
+        return 'app'
+
+
 
 class SearchView(TemplateView):
     template_name = 'search.html'
