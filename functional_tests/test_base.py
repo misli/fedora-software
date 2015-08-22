95915a2b18539b1daf9d390fb31adeef54a85bfb
pigjuliux <juliux.pigface@gmail.com>
2015-08-22 11:20:49 +0200

    Adjust some spelling and add debugging for 'test_if_carousel_displays_a_valid_image'

--- functional_tests/test_base.py
+++ functional_tests/test_base.py
@@ -41,10 +41,10 @@ class FunctionalTest(TestCase):
         Checks if the browser is able to reach fedora-software
         """
         # Kathe is the user of our example. She has heard about a new
-        # exciting software for her Fedora's workstation. She visits it
+        # exciting software for her Fedora Workstation. She visits it
         # via Firefox.
 
-        # She notices the page title mention 'Fedora-Software'
+        # She notices the page title mentions 'Fedora-Software'
         self.assertIn("Welcome to Fedora Software", self.browser.title)
 
     def test_if_featured_apps_are_displayed_correctly(self):
@@ -56,7 +56,7 @@ class FunctionalTest(TestCase):
         featured_header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertEqual("Featured apps", featured_header_text)
 
-        # She notices that there some apps appears randomly
+        # She notices that some apps appears there randomly
         app_link = self.browser.find_element_by_id("featured_app_href")
         app_link = app_link.get_attribute("href")
         app_pkgname = "None"
@@ -77,9 +77,19 @@ class FunctionalTest(TestCase):
         # She admires the big logo of the 'FeaturedApp' which is
         # currently displayed.
         carousel = self.browser.find_element_by_id("carousel-featured")
-
         carousel_html = carousel.value_of_css_property("background")
-        carousel_image = carousel_html.split('url("')[1]
+        
+        print "#################"
+        print "DEBUGGING:"
+        featured_app = self.browser.find_element_by_id("featured_app_href")
+        featured_app.get_attribute("href").split("/apps/")[1]
+        print featured_app
+        print "#################"
+        print carousel_html
+        print "#################"
+        
+        carousel_image = carousel_html.split('/', 1)[1]
+        carousel_image = "/" + carousel_image
         carousel_path = carousel_image.split('") no-repeat')[0][1:]
         project_path = Path(__file__).ancestor(2)
         fedorasoftware_app_path = project_path.child("fedora_software")
@@ -87,6 +97,4 @@ class FunctionalTest(TestCase):
                 fedorasoftware_app_path,
                 carousel_path
                 )
-
         self.assertTrue(os.path.exists(carousel_file))
-
